95493b8a41dea7eaca5d5fcf902c0f6bb819edbd
YuHyoJin-200723 <68654501+YuHyoJin-200723@users.noreply.github.com>
2020-07-24 20:47:26 +0900

    Create p.40_NameSp2.cpp

---
+++ 2020_1/20192609_ 유효진/p.40_NameSp2.cpp
@@ -0,0 +1,30 @@
+// 이름공간 기반의 함수 선언과 정의의 구분 (p.40)
+
+#include <iostream>
+
+namespace BestComImpl
+{
+	void SimpleFunc(void);
+}
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void);
+}
+
+int main(void)
+{
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
+
+void BestComImpl::SimpleFunc(void)
+{
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+	std::cout << "PromCom이 정의한 함수" << std::endl;
+}
