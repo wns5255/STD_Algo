18ac025369c3768e6a27f0b97d12739bbd9c8dfd
YuHyoJin-200723 <68654501+YuHyoJin-200723@users.noreply.github.com>
2020-07-24 20:52:31 +0900

    Create p.48_NameAlias.cpp

---
+++ 2020_1/20192609_ 유효진/p.48_NameAlias.cpp
@@ -0,0 +1,27 @@
+// 이름공간의 별칭 지정 (p.48)
+
+#include <iostream>
+using namespace std;
+
+namespace AAA
+{
+	namespace BBB
+	{
+		namespace CCC
+		{
+			int num1;
+			int num2;
+		}
+	}
+}
+
+int main(void)
+{
+	AAA::BBB::CCC::num1 = 20;
+	AAA::BBB::CCC::num2 = 30;
+
+	namespace ABC = AAA::BBB::CCC;
+	cout << ABC::num1 << endl;
+	cout << ABC::num2 << endl;
+	return 0;
+}
