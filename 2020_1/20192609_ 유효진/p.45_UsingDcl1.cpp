76b79ead54cbaa7386f8634fbc78fe88743067ac
YuHyoJin-200723 <68654501+YuHyoJin-200723@users.noreply.github.com>
2020-07-24 20:50:10 +0900

    Create p.45_UsingDcl1.cpp

---
+++ 2020_1/20192609_ 유효진/p.45_UsingDcl1.cpp
@@ -0,0 +1,19 @@
+// Using을 이용한 이름공간의 명시 (p.45)
+
+#include <iostream>
+
+namespace Hybrid
+{
+	void HybFunc(void)
+	{
+		std::cout << "So simple function!" << std::endl;
+		std::cout << "In namespace Hybrid!" << std::endl;
+	}
+}
+
+int main(void)
+{
+	using Hybrid::HybFunc;
+	HybFunc();
+	return 0;
+}
