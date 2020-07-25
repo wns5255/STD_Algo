568e304e7f1ff55ad49739456047f46c9b84f98c
YuHyoJin-200723 <68654501+YuHyoJin-200723@users.noreply.github.com>
2020-07-26 00:26:47 +0900

    Create p.69_Referen.cpp

---
+++ 2020_1/20192609_ 유효진/p.69_Referen.cpp
@@ -0,0 +1,18 @@
+// 참조자(Reference)의 이해
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num1 = 1020;
+	int &num2 = num1;
+
+	num2 = 3047;
+	cout << "VAL: " << num1 << endl;
+	cout << "REF: " << num2 << endl;
+
+	cout << "VAL: " << &num1 << endl;
+	cout << "REF: " << &num2 << endl;
+	return 0;
+}
