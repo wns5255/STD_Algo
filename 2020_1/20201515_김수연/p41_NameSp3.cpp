b87ec8f93126dabfccb8ac7542e435517128bdb8
astell601 <52621942+astell601@users.noreply.github.com>
2020-07-28 00:52:02 +0900

    Create p41_NameSp3.cpp

---
+++ 2020_1/20201515_김수연/p41_NameSp3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+namespace BestComImpl
+{
+    void SimpleFunc(void);
+}
+
+namespace BestComImpl
+{
+    void PrettyFunc(void);
+}
+
+namespace ProgComImpl
+{
+    void SimpleFunc(void);
+}
+
+int main(void)
+{
+    BestComImpl::SimpleFunc();
+    return 0;
+}
+
+void BestComImpl::SimpleFunc(void)
+{
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+    PrettyFunc();
+    ProgComImpl::SimpleFunc();
+}
