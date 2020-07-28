d4c997d8f0d3b705a8e79df2a4881e81127f7c69
shinahrim <45410363+shinahrim@users.noreply.github.com>
2020-07-28 21:06:38 +0900

    Add files via upload

---
+++ 2020_1/20170421_신아림/page90_q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   const int num = 12;
+   const int *ptr = &num;
+   const int *(&ref) = ptr;
+
+   cout<<*ptr<<endl;
+   cout<<*ref<<endl;
+
+   return 0;
+}
