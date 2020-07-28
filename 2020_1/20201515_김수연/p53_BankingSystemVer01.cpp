e10a0e0783efb4e4f6ce21518f129152ff6066d1
astell601 <52621942+astell601@users.noreply.github.com>
2020-07-28 21:40:14 +0900

    Update p53_BankingSystemVer01.cpp

--- 2020_1/20201515_김수연/p53_BankingSystemVer01.cpp
+++ 2020_1/20201515_김수연/p53_BankingSystemVer01.cpp
@@ -61,3 +61,70 @@ int main(void)
     return 0;
 }
 
+void ShowMenu(void)
+{
+    cout<<"-----Menu------"<<endl;
+    cout<<"1. 계좌개설"<<endl;
+    cout<<"2. 입 금"<<endl;
+    cout<<"3. 출 금"<<endl;
+    cout<<"4. 계좌정보 전체 출력"<<endl;
+    cout<<"5. 프로그램 종료"<<endl;
+}
+
+void MakeAccount(void)
+{
+    int id;
+    char name[NAME_LEN];
+    int balance;
+    
+    cout<<"[계좌개설]"<<endl;
+    cout<<"계좌ID: "; cin>>id;
+    cout<<"입금액: "; cin>>money;
+    
+    for(int i = 0; i<accNum; i++)
+    {
+        if(accArr[i].accID==id)
+        {
+            accArr[i].balance+=money;
+            cout<<"입금완료"<<endl<<endl;
+            return;
+        }
+    }
+    cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
+}
+      
+void WithdrawMoney(void)
+{
+    int money;
+    int id;
+    cout<<"[출   금]"<<endl;
+    cout<<"계좌ID: "; cin>>id;
+    cout<<"출금액: "; cin>>money;
+    
+    for(int i = 0; i<accNum; i++)
+    {
+        if(accArr[i].accID==id)
+        {
+            if(accArr[i].balance<money)
+            {
+                cout<<"잔액부족"<<endl<<endl;
+                return;
+            }
+            
+            accArr[i].balance-=money;
+            cout<<"출금완료"<<endl<<endl;
+            return;
+        }
+    }
+    cout<<"유효하지 않은 ID 입니다."<<endl<<endl;
+}
+
+void ShowAllAccInfo(void)
+{
+    for(int i = 0; i<<accNum; i++)
+    {
+        cout<<"계좌ID: "<<accArr[i].accID<<endl;
+        cout<<"이  름: "<<accArr[i].cusName<<endl;
+        cout<<"잔  액: "<<accArr[i].balance<<endl<<endl;
+    }
+}
