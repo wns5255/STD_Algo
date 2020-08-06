4e9edfe244004d50f1286d876e5103ba80487032
DESKTOP-1P5PELK\김태헌 <kimdenny123@naver.com>
2020-08-06 20:39:06 +0900

    백준 17392 우울한 방학 문제 해결!

--- 2020_1/20162556_김태헌/BOJ_17392.py
+++ 2020_1/20162556_김태헌/BOJ_17392.py
@@ -3,11 +3,11 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 
 result = sum(map(lambda x: int(x) + 1, sys.stdin.readline().split()))
-tmp = max(0, (m - result) / (n+1))
+tmp = int(max(0, (m - result) / (n+1)))
 if not bool(tmp):
     output = max(0, m - result)
 else:
-    output = sum(map(lambda x: x*x, range(1, int(tmp))))
-    tmp2 = m - result - int(tmp) * (n+1)
-    output += tmp2 * pow((int(tmp)+1),2)
+    output = sum(map(lambda x: x*x, range(1, tmp+1))) * (n+1)
+    tmp2 = m - result - tmp * (n+1)
+    output += tmp2 * pow((tmp+1), 2)
 print(output)
