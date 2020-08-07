5442da85e4ef2fb3585a655ed64e6e0da5d96fed
DESKTOP-1P5PELK\김태헌 <kimdenny123@naver.com>
2020-08-07 15:34:48 +0900

    자료구조를 set - 집합으로 바꿔서 성능을 올리긴 했는데 38% 쯤에서 시간초과 발생

--- 2020_1/20162556_김태헌/BOJ_19538.py
+++ 2020_1/20162556_김태헌/BOJ_19538.py
@@ -3,37 +3,39 @@ import sys
 n = int(sys.stdin.readline())
 people_dict = [None] * (n+1)
 for _ in range(n):
-    people_dict[_+1] = [False, tuple(map(int, sys.stdin.readline().split()))[:-1]]
+    people_dict[_+1] = [False, set(map(int, sys.stdin.readline().split()[:-1]))]
 
 m = int(sys.stdin.readline())
-ing = []
-for i in list(map(int, sys.stdin.readline().split())):
+ing = set()
+trust = set()
+start = set(map(int, sys.stdin.readline().split()))
+for i in start:
     people_dict[i][0] = 1
-    ing += people_dict[i][1]
+    ing.update(people_dict[i][1])
 
-ing = [n for n in list(dict.fromkeys(ing)) if not bool(people_dict[n][0])]
+ing -= start
+trust.update(start)
 
 n_iter = 2
 while len(ing) != 0:
-    change = []
-    tmp = []
+    change = set()
+    tmp = set()
+    finish = True
     for i in ing:
-        count = [_ for _ in people_dict[i][1] if not bool(people_dict[_][0])]
+        count = people_dict[i][1] - trust
         if len(count) <= (len(people_dict[i][1])/2.):
-            change.append(i)
-            tmp += count
-        else:
-            tmp.append(i)
+            change.add(i)
+            tmp.update(count)
+            finish = False
 
-    for i in change:
-        people_dict[i][0] = n_iter
-        if i in tmp:
-            tmp.remove(i)
-    tmp = list(dict.fromkeys(tmp))
-    if all(x in tmp for x in ing) and len(ing) == len(tmp):
+    if finish:
         break
-
+    for i in change:
+        trust.add(i)
+        people_dict[i][0] = n_iter
+    tmp.update(ing)
+    tmp -= change
     ing = tmp
     n_iter += 1
 
-print(*[-1 if not bool(x[0]) else x[0]-1 for x in people_dict[1:]])
+print(*map(lambda x: x[0]-1 if bool(x[0]) else -1, people_dict[1:]))
