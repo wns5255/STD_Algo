81f2b39f9a9d51e9e105ba67b7c314cc7e86f134
YuHyoJin-200723 <68654501+YuHyoJin-200723@users.noreply.github.com>
2020-08-12 20:35:41 +0900

    Update MC_Menu.cpp

--- 2020_1/20192609_ 유효진/MC_Menu.cpp
+++ 2020_1/20192609_ 유효진/MC_Menu.cpp
@@ -1,55 +1,86 @@
-#pragma once
 #include <iostream>
+#include <cstring>
+#include "Menu.h"
 using namespace std;
 
-class Burger {
-private:
-	char BurgerName[20];
-	int num; // 파는 개수
-	int BurgerPrice; // 버거 가격
-public:
-	Burger(const char* name, int n);
-	int SaleBurger();
-	void ShowData() const;
-};
+// 버거 클래스 멤버변수 초기화
+Burger::Burger(const char* name, int n) 
+{
+	strcpy_s(BurgerName, name);
+	num = n;
+	BurgerPrice = 4000;
+}
 
-class Fries {
-private:
-	int num;
-	int FriesPrice;
-public:
-	Fries(int n);
-	int SaleFries();
-	void ShowData() const;
-};
+// 버거 판매 기능
+int Burger::SaleBurger() 
+{
+	int cost = num * BurgerPrice;
+	return cost;
+}
 
-class Drink {
-private:
-	int num;
-	int DrinkPrice;
-public:
-	Drink(int n);
-	int SaleDrink();
-	void ShowData() const;
-};
+// 버거 주문 출력
+void Burger::ShowData() const
+{
+	cout << BurgerName << "버거 " << num << "개를 주문하셨습니다." << endl;
+}
 
-class Icecream {
-private:
-	int num;
-	int IcecreamPrice;
-public:
-	Icecream(int n);
-	int SaleIcecream();
-	void ShowData() const;
-};
+// 감자튀김
+Fries::Fries(int n) 
+{
+	num = n;
+	FriesPrice = 2000;
+}
+int Fries::SaleFries() 
+{
+	int cost = num * FriesPrice;
+	return cost;
+}
+void Fries::ShowData() const 
+{
+	cout << "감자튀김 " << num << "개를 시키셨습니다." << endl;
+}
 
-class Set {
-private:
-	Burger b;
-	Fries f;
-	Drink d;
-public:
-	Set(const char* name);
-	int SaleSet();
-	void ShowData();
-};
+// 음료
+Drink::Drink(int n) 
+{
+	num = n;
+	DrinkPrice = 1000;
+}
+int Drink::SaleDrink() 
+{
+	int cost = num * DrinkPrice;
+	return cost;
+}
+void Drink::ShowData() const 
+{
+	cout << "음료 " << num << "개를 시키셨습니다." << endl;
+}
+
+// 아이스크림
+Icecream::Icecream(int n) 
+{
+	num = n;
+	IcecreamPrice = 1500;
+}
+int Icecream::SaleIcecream() 
+{
+	int cost = num * IcecreamPrice;
+	return cost;
+}
+void Icecream::ShowData() const 
+{
+	cout << "아이스크림 " << num << "개를 시키셨습니다." << endl;
+}
+
+// 세트
+Set::Set(const char* name) : b(name, 1), f(1), d(1)
+{ }
+int Set::SaleSet() 
+{
+	int cost = b.SaleBurger() + f.SaleFries() + d.SaleDrink() - 1000;
+	return cost;
+}
+void Set::ShowData() 
+{
+	cout << "버거 세트를 주문하셨습니다." << endl;
+}
