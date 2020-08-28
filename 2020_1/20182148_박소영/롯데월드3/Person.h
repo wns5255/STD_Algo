ed8155513ce153a47e7d69347de4e508dd8fdbc7
so0126 <66772193+so0126@users.noreply.github.com>
2020-08-28 10:29:10 +0900

    Add files via upload

---
+++ 2020_1/20182148_박소영/롯데월드3/Person.h
@@ -0,0 +1,27 @@
+#ifndef _PERSON_H_
+#define _PERSON_H_
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include "Menu.h"
+#include "Ride.h"
+
+class Person
+{
+private:
+    Ride ride;
+    int num, age, height, money, min, WPC;
+    char* menu; // Menu로 보낼 메뉴 이름 값
+    int num2, EatPrice;   // Menu로 보낼 메뉴 개수 값, 식비
+    char type[100];
+public:
+    //void Buy_Food(const OfferMenu& menu, int pay);
+    Person() { money = 10000, min = 10, num = 1, age = 14, height = 150; }
+    Person(int myMoney, int n, int m, int myAge, int h) : money(myMoney), num(n), min(m), age(myAge), height(h){};   // 음식점 , 디폴트
+    Person(int myMoney, int n, int m, int myAge) :money(myMoney), num(n), min(m), age(myAge) {};
+    void PAY();
+    void TAKE();
+    void ChoiceMenu(Menu& eat, char* WantMenu, int n2);
+};
+
+
+#endif;
